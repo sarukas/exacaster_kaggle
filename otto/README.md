28e0cfbcc03a0905808983f4a20e9343a2b4cd47
rokastamosiunas <trokas@gmail.com>
2015-04-10 10:36:52 +0300

    Keli nauji h2o modeliai, rezultatai readme

--- otto/README.md
+++ otto/README.md
@@ -10,15 +10,25 @@ Reikia pasiruošti kokybišką template kurį galėsime naudoti tolesniam modeli
 * Tvarkingas `test.csv` ir `train.csv` skaitymas
 * Vidinis train ir test split su keičiamais parametrais ir galimybe praleisti
 * Modeliavimo blokas
-* Logloss estimator
+* Logloss estimator tiek `local_train`, tiek `local_test` vertinimui
 * Tvarkingas rašymas į `submission.csv`
 
-Pabandžiau greitai sumesti į `template.R`. Yra vienas svarbus pastebėjimas - šiuo metu padaryta classification algoritmams. Regresijos algoritmo naudojimo pavyzdys `h2o.R` vėliau galės būti perdarytas į *template*. Reiktų jį ir komentarais papildyti.
+Pabandžiau greitai sumesti į `template.R`. Yra vienas svarbus pastebėjimas - šiuo metu padaryta classification algoritmams. Regresijos algoritmo naudojimo pavyzdys `h2o.R` vėliau galės būti perdarytas į *template*. 
 
 **Svarbu:** modeliai su tiek kintamųjų labai daug sveria, taigi bandykite atsargiai.
 
 ## Rezultatai
 
-Kaggle *uploads* rezultatai:
+`h2o.R` išbandyti modeliai (be optimizavimo):
+* Deep Learning Neural Networks
+* Gradient Boosted Machines
+* Single-Node Random Forest
+* Random Forest
 
-1. Bandymas su `h2o.R`. Realiai tikslas buvo tik užmesti akį ar viskas veikia. *Score: 0.57369*.
+| Modelis | local_train | local_test | kaggle |
+|---------|-------------|------------|--------|
+| h2o.deeplearning benchmark | 0.4062493 | 0.6180687 | 0.57369 |
+| h2o.gbm benchmark | 0.7232466 | 0.7653265 ||
+| h2o.SpeeDRF benchmark | 0.4176488 | 0.9722669 ||
+| h2o.randomForest benchmark | 0.22919 | 0.769467 ||
+
--- otto/h2o.R
+++ otto/h2o.R
@@ -2,22 +2,7 @@ library(data.table)
 library(h2o)
 localH2O = h2o.init()
 
-# Skaitome duomenis
-DT <- fread("train.csv")
-DT[, target := as.factor(target)]
-n <- nrow(DT)
-
-# Sukuriame training set, šiuo atveju tiesiog sumaišiau eilutes
-# Reikia padaryti split taip kaip template.R
-set.seed(42)
-index <- sample(n)
-DT = DT[index,]
-DT.hex <- as.h2o(localH2O, DT)
-
-# Neural network modelis
-deep <- h2o.deeplearning(x=2:94, y=95, data = DT.hex, classification = TRUE, nfolds = 10, override_with_best_model = TRUE)
-
-# Logloss estimation 
+# Logloss estimation iš kaggle
 MultiLogLoss <- function(act, pred)
 {
   eps = 1e-15;
@@ -29,22 +14,65 @@ MultiLogLoss <- function(act, pred)
   return(ll);
 }
 
-prediction <- h2o.predict(deep, DT.hex)
+# Skaitome duomenis
+train <- fread("train.csv")
+test <- fread("test.csv")
+train[, target := as.factor(target)]
+n <- nrow(train)
+
+# Sukuriame training set, šiuo atveju tiesiog sumaišiau eilutes
+# Reikia padaryti split taip kaip template.R
+set.seed(42)
+m <- round(n*0.75)
+index <- sample(n, size = m)
+
+local_train = train[index,]
+local_test = train[-index,]
+local_train.hex <- as.h2o(localH2O, local_train)
+local_test.hex <- as.h2o(localH2O, local_test)
+
+################################
+#     Modelling part           #
+################################
+
+model <- h2o.deeplearning(x=2:94, y=95, data = local_train.hex, classification = TRUE, nfolds = 10, override_with_best_model = TRUE, seed = 42)
+model <- h2o.gbm(x=2:94, y=95, data = local_train.hex, nfolds = 10, n.trees = 20)
+model <- h2o.SpeeDRF(x=2:94, y=95, data = local_train.hex, nfolds = 10, classification = TRUE, seed = 42)
+model <- h2o.randomForest(x=2:94, y=95, data = local_train.hex, depth = 25, nfolds = 10, classification = TRUE, seed = 42)
+
+################################
+#         Rezultatai           #
+################################
+
+# local_train rezultatai
+prediction <- h2o.predict(model, local_train.hex)
 results <- data.table(as.data.frame(prediction))
 results[, predict := NULL]
-#real_labels <- data.table(cbind(model.matrix(~ 0 + target, DT)))
+results_matrix <- as.matrix(results)
+actual_matrix <- model.matrix(~ 0 + target, local_train)
+MultiLogLoss(actual_matrix, results_matrix)
 
-actual_matrix <- model.matrix(~ 0 + target, DT)
+# local_test rezultatai
+prediction <- h2o.predict(model, local_test.hex)
+results <- data.table(as.data.frame(prediction))
+results[, predict := NULL]
+results_matrix <- as.matrix(results)
+actual_matrix <- model.matrix(~ 0 + target, local_test)
+MultiLogLoss(actual_matrix, results_matrix)
 
-MultiLogLoss(actual_matrix, as.matrix(results))
-# deeplearning benchmark: local score 0.38897, kaggle score 0.57369
+
+#                               local_train score   local_test score    kaggle score
+# h2o.deeplearning benchmark    0.4062493           0.6180687           0.57369
+# h2o.gbm benchmark             0.7232466           0.7653265
+# h2o.SpeeDRF benchmark         0.4176488           0.9722669
+# h2o.randomForest benchmark    0.22919             0.769467
+
 
 # Prireikus galima išsaugoti modelį ir jį atkurti
 # path <- h2o.saveModel(object = deep)
 # h2o.loadModel(localH2O, path)
 
 # Pritaikome modelį test.csv
-test <- fread("test.csv")
 index <- test[, id]
 test.hex <- as.h2o(localH2O, test)
 prediction <- h2o.predict(deep, test.hex)
@@ -56,8 +84,6 @@ setnames(results, "predict", "id")
 
 #Surašome viską į failą
 write.table(results, file = "deep_benchmark.csv", sep = ",", quote = FALSE, row.names = FALSE)
-
-
 
 
 
